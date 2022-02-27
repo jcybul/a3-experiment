f26f71592712fd4fa8c05e8c662d41ab91f7725f
jcybul <46726471+jcybul@users.noreply.github.com>
2022-02-27 01:27:18 -0500

    Revert "Merge branch 'gh-pages' of https://github.com/jcybul/a3-experiment into gh-pages"
    This reverts commit 36bde4f473cf35a3ea204c7dab0653a0e210fa72, reversing
    changes made to 1a53216c879fc605cb5d5201a67a7873456899a1.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const http = require('http');
 const fs = require('fs');
 const mime = require('mime');
-const sanitize = require('sanitize-filename');
 
 
 const defaultPort = 3000;
@@ -38,7 +37,7 @@ function saveResultsData(data, res) {
     }
 
     // Use the simple approach of just dumping everything in a csv then sorting through it later
-    const csvLine = extractCSVLine(JSON.parse(data), columns);
+    const csvLine = extractCSVLine(data, columns);
     console.log(`Received data line "${csvLine}" from message ${JSON.stringify(data)}`);
     fs.appendFile(dataFile, csvLine + "\n", function (err) {
         if (err) {
@@ -79,15 +78,13 @@ function sendFile(response, filename) {
 
 
 function handleRequest(req, res) {
-    console.log(`${req.method}: ${req.url}`);
-
     // Handle static file serving, but probably won't be needed since we are using github pages
     // Could also be used to fetch the results from the server
     if (req.method === "GET") {
         if (req.url === '/') {
             return sendFile(res, 'public/index.html');
         } else {
-            return sendFile(res, "public/" + sanitize(req.url));
+            return sendFile(res, "public" + req.url);
         }
     }
 
