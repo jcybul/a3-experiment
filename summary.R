f964069fcaf4bb59d5ac33f1951fb47076877c36
jcybul <46726471+jcybul@users.noreply.github.com>
2022-03-03 12:14:52 -0500

    Update summary.R

--- summary.R
+++ summary.R
@@ -4,9 +4,18 @@ file <- 'C:/Users/19292/Documents/GitHub/a3-experiment/error.csv'
 d <- read.csv(file)
 d
 class(d)
-d <- sort (d, decreasing = FALSE)
-d
+
 # Orientation follows the discrete axis
 ggplot(d,aes(x=error,y=type)) +
-  geom_point(alpha=0.6) + xlim(0, 6) +
-  stat_summary( fun.data = "mean_cl_boot",colour = "Blue", size = 1)
+  geom_point(alpha=0.2) + coord_cartesian(xlim=c(0, 7)) +ggtitle("95% confidence interval for mean bootstrap based on chart type and feedback")+
+  stat_summary( fun.data = "mean_cl_boot",colour = "Blue", size = 1,fun.args=list(conf.int=.95)
+)
+
+stat_sum_df <- function(fun, geom="crossbar", ...) {
+  stat_summary(fun.data = fun, colour = "blue", geom = geom, width = 0.2, ...)
+}
+p <- ggplot(d,aes(x=error,y=type)) + geom_point(alpha=0.2) + coord_cartesian(xlim=c(0, 7)) +ggtitle("95% confidence interval for mean bootstrap based on chart type and feedback")
+# The crossbar geom needs grouping to be specified when used with
+# a continuous x axis.
+p + stat_sum_df("mean_cl_boot", mapping = aes(group = type))
+
